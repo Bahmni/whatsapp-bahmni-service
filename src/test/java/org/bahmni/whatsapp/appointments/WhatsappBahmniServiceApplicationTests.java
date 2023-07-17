1b08d03f596993af6b693029bb6defb06ec48ff6
aniketbhatia28 <aniketbhatia28@gmail.com>
2023-07-17 13:30:56 +0530

    initial commits

---
+++ src/test/java/org/bahmni/whatsapp/appointments/WhatsappBahmniServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bahmni.whatsapp.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappBahmniServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
