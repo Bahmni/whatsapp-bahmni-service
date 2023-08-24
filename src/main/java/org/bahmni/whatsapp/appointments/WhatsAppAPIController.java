c3a9593f063297412ffa3bdc12be040e2555fa35
aniketbhatia28 <aniketbhatia28@gmail.com>
2023-08-24 18:22:24 +0530

    [Aniket] | BAH-3116 | Add. Basic Integration of WhatsApp Cloud API with Microservice

---
+++ src/main/java/org/bahmni/whatsapp/appointments/WhatsAppAPIController.java
@@ -0,0 +1,99 @@
+package org.bahmni.whatsapp.appointments;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class WhatsAppAPIController {
+
+    public String sendMessage(String phoneNumberId, String token, JSONObject data) {
+        String url = "https://graph.facebook.com/v17.0/" + phoneNumberId + "/messages?access_token=" + token;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<String> requestEntity = new HttpEntity<>(data.toString(), headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, requestEntity, String.class);
+
+        String jsonResponse = responseEntity.getBody();
+
+        JSONObject responseJson = new JSONObject(jsonResponse);
+
+        String message_id = "";
+        JSONArray messages = responseJson.getJSONArray("messages");
+        if (messages.length() > 0) {
+            JSONObject msg = messages.getJSONObject(0);
+            message_id = msg.getString("id");
+        }
+
+        return message_id;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "webhook")
+    public ResponseEntity<String> verifyWebhook(@RequestParam("hub.mode") String mode,
+                                                @RequestParam("hub.challenge") String challenge,
+                                                @RequestParam("hub.verify_token") String token) {
+
+        if (mode.equals("subscribe") && token.equals("abc123")) {
+            return new ResponseEntity<>(challenge, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Verification token or mode mismatch", HttpStatus.FORBIDDEN);
+        }
+    }
+
+    @RequestMapping( method = RequestMethod.POST, value = "webhook")
+    public ResponseEntity<String> notificationHandler(@RequestBody String requestBody){
+
+        JSONObject body = new JSONObject(requestBody);
+
+        if (!body.has("object")) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if (!body.has("entry") ||
+                body.getJSONArray("entry").length() <= 0 ||
+                !body.getJSONArray("entry").getJSONObject(0).has("changes") ||
+                body.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").length() <= 0 ||
+                !body.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).has("value") ||
+                !body.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).getJSONObject("value").has("messages") ||
+                body.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).getJSONObject("value").getJSONArray("messages").length() <= 0
+        ) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        JSONObject msg = body.getJSONArray("entry")
+                .getJSONObject(0)
+                .getJSONArray("changes")
+                .getJSONObject(0)
+                .getJSONObject("value")
+                .getJSONArray("messages")
+                .getJSONObject(0);
+
+        if (msg.getString("type").equals("text")) {
+            String from = msg.getString("from"); // Phone Number of Patient
+            String reply_message = "Thanks for contacting Bahmni, Appointments Booking Feature will be live soon."; // Reply message to sent back to Patient
+
+            JSONObject data = new JSONObject();
+            data.put("messaging_product", "whatsapp");
+            data.put("to", from);
+            data.put("type", "text");
+
+            JSONObject textBody = new JSONObject();
+            textBody.put("body", reply_message);
+
+            data.put("text", textBody);
+
+            String phone_number_id = "109855275525315";
+            String token = "WhatsApp_Access_Token";
+            String wa_id = sendMessage(phone_number_id, token, data);
+
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
