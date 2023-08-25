6845009ce569043df89b7f19d74e8d03b9bfb877
aniketbhatia28 <aniketbhatia28@gmail.com>
2023-08-25 18:38:37 +0530

    [Aniket] | BAH-3138 | Add Authentication with OpenMRS to fetch Cookies

--- src/main/java/org/bahmni/whatsapp/appointments/WhatsAppAPIController.java
+++ src/main/java/org/bahmni/whatsapp/appointments/WhatsAppAPIController.java
@@ -2,6 +2,7 @@ package org.bahmni.whatsapp.appointments;
 
 import org.bahmni.webclients.ClientCookies;
 import org.bahmni.whatsapp.appointments.contract.patient.OpenMRSPatientFullRepresentation;
+import org.bahmni.whatsapp.appointments.services.OpenmrsLoginImpl;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +17,11 @@ import java.text.ParseException;
 @RestController
 public class WhatsAppAPIController {
 
-//    @RequestMapping(method = RequestMethod.GET, value = "test")
-//    public String getMessage() {
-//        return "hello world";
-//    }
     @Autowired
-OpenMRSService openMRSService;
+    OpenMRSService openMRSService;
 
+    @Autowired
+    OpenmrsLoginImpl openmrsLogin;
 
     public String sendMessage(String phoneNumberId, String token, JSONObject data) throws IOException, ParseException {
         String url = "https://graph.facebook.com/v17.0/" + phoneNumberId + "/messages?access_token=" + token;
@@ -32,24 +31,20 @@ OpenMRSService openMRSService;
 
         HttpEntity<String> requestEntity = new HttpEntity<>(data.toString(), headers);
 
-//        openMRSService.getConnection();
-//        ClientCookies cookies = openMRSService.getCookies();
-//        System.out.println(cookies);
+        openmrsLogin.getConnection();
+        ClientCookies cookies = openmrsLogin.getCookies();
 
-        OpenMRSPatientFullRepresentation patient_info = openMRSService.getPatientFR("https://demo-lite.mybahmni.in/openmrs/ws/atomfeed/patient/recent");
-        System.out.println(patient_info);
+//        OpenMRSPatientFullRepresentation patient_info = openMRSService.getPatientFR("https://demo-lite.mybahmni.in/openmrs/ws/atomfeed/patient/recent");
+//        System.out.println(patient_info);
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> responseEntity = restTemplate.postForEntity(url, requestEntity, String.class);
 
         String jsonResponse = responseEntity.getBody();
 
-        //Parsing the json response
         JSONObject responseJson = new JSONObject(jsonResponse);
-        System.out.println("response json: " + responseJson);
 
         String message_id = "";
-        // Extract the message ID (messages is an array)
         JSONArray messages = responseJson.getJSONArray("messages");
         if (messages.length() > 0) {
             JSONObject msg = messages.getJSONObject(0);
@@ -59,37 +54,26 @@ OpenMRSService openMRSService;
         return message_id;
     }
 
-    //Webhook Verification and Configuration
     @RequestMapping(method = RequestMethod.GET, value = "webhook")
     public ResponseEntity<String> verifyWebhook(@RequestParam("hub.mode") String mode,
                                                 @RequestParam("hub.challenge") String challenge,
                                                 @RequestParam("hub.verify_token") String token) {
-        System.out.println(mode);
-        System.out.println(challenge);
-        System.out.println(token);
+
         if (mode.equals("subscribe") && token.equals("abc123")) {
-            System.out.println("Webhook Verified");
             return new ResponseEntity<>(challenge, HttpStatus.OK);
         } else {
             return new ResponseEntity<>("Verification token or mode mismatch", HttpStatus.FORBIDDEN);
         }
     }
 
-    //Handling of Notifications about patient's messages or message sent status changes from Cloud API
     @RequestMapping( method = RequestMethod.POST, value = "webhook")
     public ResponseEntity<String> notificationHandler(@RequestBody String requestBody) throws IOException, ParseException {
-        System.out.println("requestBody: " + requestBody);
 
-        // Parse the JSON request body
         JSONObject body = new JSONObject(requestBody);
-        System.out.println("Json body: " + body);
 
         if (!body.has("object")) {
-            // Return a '404 Not Found' if event is not from a WhatsApp API
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
-
-        //For reference of how a Notification payload object looks like: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/components#webhooks-notification-payload-reference
 
         if (!body.has("entry") ||
                 body.getJSONArray("entry").length() <= 0 ||
@@ -99,8 +83,6 @@ OpenMRSService openMRSService;
                 !body.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).getJSONObject("value").has("messages") ||
                 body.getJSONArray("entry").getJSONObject(0).getJSONArray("changes").getJSONObject(0).getJSONObject("value").getJSONArray("messages").length() <= 0
         ) {
-            //This is the case when notification payload has details about message status and not the message itself.
-            //i.e. if a message is sent, delivered or read by the patient or if there is an error in sending the message.
             return new ResponseEntity<>(HttpStatus.OK);
         }
 
@@ -112,10 +94,9 @@ OpenMRSService openMRSService;
                 .getJSONArray("messages")
                 .getJSONObject(0);
 
-        //check if a message sent by the patient is of type text or not.
         if (msg.getString("type").equals("text")) {
-            String from = msg.getString("from"); // Phone Number of Patient
-            String reply_message = "Thanks for contacting Bahmni, Appointments Booking Feature will be live soon."; // Reply message to sent back to Patient
+            String from = msg.getString("from");
+            String reply_message = "Thanks for contacting Bahmni, Appointments Booking Feature will be live soon.";
 
             JSONObject data = new JSONObject();
             data.put("messaging_product", "whatsapp");
@@ -126,21 +107,10 @@ OpenMRSService openMRSService;
             textBody.put("body", reply_message);
 
             data.put("text", textBody);
-            System.out.println("Data blob: " + data);
-
-            //The data blob would look something like this (for reference):
-            //                data = {
-            //                        messaging_product: "whatsapp",
-            //                        to: from,
-            //                        type: "text",
-            //                        text: { body: reply_message }
-            //                };
 
             String phone_number_id = "109855275525315";
-            String token = "EAAJLW2eCmuQBO11hvQZCGxyZATZBZBAj4EJCZCCFZC9WKCf9HwZCwS4eqeF0MBwfE07u4EoeMFpZC5NBVmFHB6NMjgh4w6XL3XACrUzwpn8rdr75MFbACE88WMnyGfPIhlInPU8V55aPFRLNR740XtykoaraUF2m2dsloUFDZBSHQ94oZCEulZCnZA33s79gSrZAUe68zVJGZAlHkyynozbNZCfzD7cmOIWGN0ZD";
+            String token = "EAAJLW2eCmuQBOxEgy8ZAwGUgd4UZBcoPhZBkSKJGhtbZAvdpW6GLL3tE90nFd9VO4WBySlH9o3pH7ZAmNiWIEdHlc1l0UAqBpsQAibtKU4ZAELF75rSUttKCwuBJuMNEcynFeArbVZCXMesFsON0NoVoIMdJzq6bMhIGnzgR3bdeuWUVGGqZAS3UXyjOYvAz58uuyHvoj4DMBVZBpWogItuSp1qKo0jMZD";
             String wa_id = sendMessage(phone_number_id, token, data);
-
-            System.out.println("whatsapp message id: " + wa_id);
         }
 
         return new ResponseEntity<>(HttpStatus.OK);
